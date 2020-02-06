2f0fcff490f128743093c78b209ab9af8479f4af
William Ntumba <ntumba_william@yahoo.fr>
2020-02-06 11:54:22 -0500

    Initial Import Function

--- src/frontend/src/app/imports/imports.component.ts
+++ src/frontend/src/app/imports/imports.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
 
 @Component({
   selector: 'app-imports',
@@ -12,9 +15,31 @@ export class ImportsComponent implements OnInit {
   ngOnInit() {
   }
 
-  import_file() {
-    // Implement CSV file import functionality
+  // import_file() {
+  //   // Implement CSV file import functionality
+  // }
+  data: AOA = [[], []];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
 
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
   }
 }
